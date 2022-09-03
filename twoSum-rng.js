8215315ea458619a587c3a077bd30bff6c551945
Dion Hobdy <devdionhobdy@gmail.com>
2022-09-02 21:35:59 -0700

    Update twoSum-rng.js

--- twoSum-rng.js
+++ twoSum-rng.js
@@ -1,16 +1,11 @@
 let randomInt = (max) => {
     return Math.floor(Math.random() * max);
 }
-
 let nums = [];
 let target = randomInt(10);
-
 for (x = 0; x < 8; x++) {
     nums.push(randomInt(10));
 }
-
-console.log(target);
-
 for (i = 0; i < nums.length; i++) {
     for (y = 0; y < nums.length; y++) {
         if (nums[i] + nums[y] == target) {
