c015a934a753a397c2fec03cbd401559eb31e701
Dion Hobdy <devdionhobdy@gmail.com>
2022-09-03 01:20:31 -0700

    Update twoSum-rng.js

--- twoSum-rng.js
+++ twoSum-rng.js
@@ -1,23 +1,19 @@
-let randomInt = (max) => { return Math.floor(Math.random() * max); }
-let arr = [];
-let nums = [];
-let target = randomInt(10);
-for (i = 0; i < 8; i++) { nums.push(randomInt(10)); }
+let randomInt = (max) => { return Math.floor(Math.random() * max) }
+let nums = []
+let target = randomInt(10)
+for (i = 0; i < 8; i++) { nums.push(randomInt(10)) }
 let twoSum = () => {
-    let hash = {};
-    let caught = false;
+    let hash = {}
+    let caught = false
     for (i = 0; i < nums.length; i++) {
         if (nums[i] == target) {
-            hash[nums.indexOf(i)] = i;
-            caught = true;
-            break;
-        } else { hash[nums.indexOf(i)] = i; }
+            hash[nums[i]] = i
+            caught = true
+            break
+        } else { hash[nums[i]] = i }
     }
-    if (caught !== true) {
-        for (j = 0; j < nums.length; j++) { 
-            let comp = target  - nums[j];
-            if (hash.hasOwnProperty(comp) && hash[comp] !== j) { return [hash[comp], j]; }
-        }
+    for (j = 0; j < nums.length; j++) { 
+        let comp = target  - nums[j] 
+        if (hash.hasOwnProperty(comp) && hash[comp] !== j) { return [hash[comp], j] }
     }
-    return null;
 }
