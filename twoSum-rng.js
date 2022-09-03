e4fa7b9f79fe9401ec46a8be83dc9366cf123d48
Dion Hobdy <devdionhobdy@gmail.com>
2022-09-02 21:34:04 -0700

    Create twoSum-rng.js

---
+++ twoSum-rng.js
@@ -0,0 +1,22 @@
+let randomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+let nums = [];
+let target = randomInt(10);
+
+for (x = 0; x < 8; x++) {
+    nums.push(randomInt(10));
+}
+
+console.log(target);
+
+for (i = 0; i < nums.length; i++) {
+    for (y = 0; y < nums.length; y++) {
+        if (nums[i] + nums[y] == target) {
+            let arr = [];
+            arr.push(nums[i], nums[y]);
+            console.log(arr);
+        }
+    }
+}
