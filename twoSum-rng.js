a73afd48d8caffce567fa9268b8fd47971a47639
Dion Hobdy <devdionhobdy@gmail.com>
2022-09-03 00:36:31 -0700

    Update twoSum-rng.js

--- twoSum-rng.js
+++ twoSum-rng.js
@@ -1,26 +1,26 @@
-let randomInt = (max) => {
-    return Math.floor(Math.random() * max)
-}
+let randomInt = (max) => { return Math.floor(Math.random() * max) }
 let arr = []
 let nums = []
 let target = randomInt(10)
-for (x = 0; x < 8; x++) {
-    nums.push(randomInt(10))
-}
+for (i = 0; i < 8; i++) { nums.push(randomInt(10)) }
 console.log(`target - ${target}`)
 console.log(nums)
-for (i = 0; i < nums.length; i++) {
-    if (nums[i] == target) {
-        arr.push(nums.indexOf(i))
-        console.log(arr)
-        break;
-    } else {
-        for (y = 0; y < nums.length; y++) {
-            if (nums[i] + nums[y] == target) {
-                arr.push(nums.indexOf(i), nums.indexOf(y))
-                console.log(arr)
-            }
-        }
+let twoSum = () => {
+    let hash = {}
+    let caught = false
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] == target) {
+            hash[nums.indexOf(i)] = i
+            let caught = true
+            break
+        } else { hash[nums.indexOf(i)] = i }
     }
+    if (caught !== true) {
+        for (j = 0; j < nums.length; j++) { let comp = target  - nums[j] }
+        if (hash.hasOwnProperty(comp) && hash[comp] !== j) { return [hash[comp], j] }
+    }
+    return null
 }
-if (arr.length === 0) { console.log('err - arr is empty') }
+// if (arr.length >= 1) { console.log(twoSum()) } else { console.log('err - arr is empty') }
+
+console.log(twoSum())
